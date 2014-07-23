c698a3ad7ed02082a3282d233ea48a4de4370d3b
Hui Zhang <hui.zhang@oregonstate.edu>
2014-07-22 22:32:22 -0700

    change font family to Gudea, make start page text h1

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,5 @@
         <div id="footer">
-        <a href="http://oregonstate.edu/about/copyright">Copyright</a>© 2013 Oregon State University. 
+        <a href="http://oregonstate.edu/about/copyright">Copyright</a>© 2014 Oregon State University. 
 	Developed by <a href="http://cdss.library.oregonstate.edu">Center for Digital Scholarship and Services</a>. Powered by Easydeposit, an open source SWORD client.
         </div>
     </body>
--- application/views/header.php
+++ application/views/header.php
@@ -6,15 +6,16 @@
         <title><?php echo $page_title; ?></title>
         <link rel='stylesheet' type='text/css' media='all' href='<?php echo base_url(); ?>css/style.css' />
         <link rel="SHORTCUT ICON" href="favicon.ico">
-        <link href='http://fonts.googleapis.com/css?family=Gueda' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Gudea' rel='stylesheet' type='text/css'>
         <?php if (!empty($javascript)) { foreach ($javascript as $js): ?>
         <script type="text/javascript" src="<?php echo base_url(); ?>js/<?php echo $js; ?>"></script>
         <?php endforeach; } ?>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
     </head>
     <body>
         <div id="header">
         <a href="http://www.oregonstate.edu" id="link"></a>
-	<h1><a href="http://osulibrary.oregonstate.edu/">OSU Libraries and Press</a><p>
+	<h1><p><a href="http://osulibrary.oregonstate.edu/">OSU Libraries and Press</a></p>
 	<a href="<?php echo base_url(); ?>">Open Access Article Deposit and Waiver Form</a>
         <hr style="width:80%; text-align:left;margin-left:0">
         <p>
